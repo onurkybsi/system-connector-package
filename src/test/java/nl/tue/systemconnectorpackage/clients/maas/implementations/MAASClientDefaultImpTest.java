56f200307ff93d72f00f22997fb6311b6a4939a3
onurkybsi <onurbpm@outlook.com>
2021-12-15 16:46:11 +0100

    MAASClientDefaultImpTest was completed

--- src/test/java/nl/tue/systemconnectorpackage/clients/maas/implementations/MAASClientDefaultImpTest.java
+++ src/test/java/nl/tue/systemconnectorpackage/clients/maas/implementations/MAASClientDefaultImpTest.java
@@ -19,130 +19,209 @@ import nl.tue.systemconnectorpackage.clients.maas.ModelCrawlerClient;
 
 public class MAASClientDefaultImpTest {
     @Test
-    public void MAASClientDefaultImpTest_Throws_InvalidParameterException_If_Given_ArrowheadHelper_Parameter_Is_Null() {
+    public void MAASClientDefaultImp_Throws_InvalidParameterException_If_Given_ArrowheadHelper_Parameter_Is_Null() {
         // Arrange
-        RepositoryManagerClient repositoryManagerClient = Mockito.mock(RepositoryManagerClient.class);
-        ModelFilterClient modelFilterClient = Mockito.mock(ModelFilterClient.class);
-        ModelTransformerClient modelTransformerClient = Mockito.mock(ModelTransformerClient.class);
+        RepositoryManagerClient mockRepositoryManagerClient = Mockito.mock(RepositoryManagerClient.class);
+        ModelFilterClient mockModelFilterClient = Mockito.mock(ModelFilterClient.class);
+        ModelTransformerClient mockModelTransformerClient = Mockito.mock(ModelTransformerClient.class);
         ModelCrawlerClient modelCrawlerClient = Mockito.mock(ModelCrawlerClient.class);
-        String systemDefinitionListResourcePath = "systemDefinitionListResourcePath";
+        String mockSystemDefinitionListResourcePath = "systemDefinitionListResourcePath";
         // Act and Assert
-        Assertions.assertThatThrownBy(() -> new MAASClientDefaultImp(null, repositoryManagerClient,
-                modelFilterClient, modelTransformerClient, modelCrawlerClient, systemDefinitionListResourcePath))
+        Assertions.assertThatThrownBy(() -> new MAASClientDefaultImp(null, mockRepositoryManagerClient,
+                mockModelFilterClient, mockModelTransformerClient, modelCrawlerClient,
+                mockSystemDefinitionListResourcePath))
                 .isInstanceOf(InvalidParameterException.class)
                 .hasMessageContaining("MAASClientDefaultImp parameters are not valid!");
     }
 
     @Test
-    public void MAASClientDefaultImpTest_Throws_InvalidParameterException_If_Given_RepositoryManagerClient_Parameter_Is_Null() {
+    public void MAASClientDefaultImp_Throws_InvalidParameterException_If_Given_RepositoryManagerClient_Parameter_Is_Null() {
         // Arrange
-        ArrowheadHelper arrowheadHelper = Mockito.mock(ArrowheadHelper.class);
-        ModelFilterClient modelFilterClient = Mockito.mock(ModelFilterClient.class);
-        ModelTransformerClient modelTransformerClient = Mockito.mock(ModelTransformerClient.class);
+        ArrowheadHelper mockArrowheadHelper = Mockito.mock(ArrowheadHelper.class);
+        ModelFilterClient mockModelFilterClient = Mockito.mock(ModelFilterClient.class);
+        ModelTransformerClient mockModelTransformerClient = Mockito.mock(ModelTransformerClient.class);
         ModelCrawlerClient modelCrawlerClient = Mockito.mock(ModelCrawlerClient.class);
-        String systemDefinitionListResourcePath = "systemDefinitionListResourcePath";
+        String mockSystemDefinitionListResourcePath = "systemDefinitionListResourcePath";
         // Act and Assert
-        Assertions.assertThatThrownBy(() -> new MAASClientDefaultImp(arrowheadHelper, null,
-                modelFilterClient, modelTransformerClient, modelCrawlerClient, systemDefinitionListResourcePath))
+        Assertions.assertThatThrownBy(() -> new MAASClientDefaultImp(mockArrowheadHelper, null,
+                mockModelFilterClient, mockModelTransformerClient, modelCrawlerClient,
+                mockSystemDefinitionListResourcePath))
                 .isInstanceOf(InvalidParameterException.class)
                 .hasMessageContaining("MAASClientDefaultImp parameters are not valid!");
     }
 
     @Test
-    public void MAASClientDefaultImpTest_Throws_InvalidParameterException_If_Given_ModelFilterClient_Parameter_Is_Null() {
+    public void MAASClientDefaultImp_Throws_InvalidParameterException_If_Given_ModelFilterClient_Parameter_Is_Null() {
         // Arrange
-        ArrowheadHelper arrowheadHelper = Mockito.mock(ArrowheadHelper.class);
-        RepositoryManagerClient repositoryManagerClient = Mockito.mock(RepositoryManagerClient.class);
-        ModelTransformerClient modelTransformerClient = Mockito.mock(ModelTransformerClient.class);
+        ArrowheadHelper mockArrowheadHelper = Mockito.mock(ArrowheadHelper.class);
+        RepositoryManagerClient mockRepositoryManagerClient = Mockito.mock(RepositoryManagerClient.class);
+        ModelTransformerClient mockModelTransformerClient = Mockito.mock(ModelTransformerClient.class);
         ModelCrawlerClient modelCrawlerClient = Mockito.mock(ModelCrawlerClient.class);
-        String systemDefinitionListResourcePath = "systemDefinitionListResourcePath";
+        String mockSystemDefinitionListResourcePath = "systemDefinitionListResourcePath";
         // Act and Assert
-        Assertions.assertThatThrownBy(() -> new MAASClientDefaultImp(arrowheadHelper, repositoryManagerClient,
-                null, modelTransformerClient, modelCrawlerClient, systemDefinitionListResourcePath))
+        Assertions.assertThatThrownBy(() -> new MAASClientDefaultImp(mockArrowheadHelper, mockRepositoryManagerClient,
+                null, mockModelTransformerClient, modelCrawlerClient, mockSystemDefinitionListResourcePath))
                 .isInstanceOf(InvalidParameterException.class)
                 .hasMessageContaining("MAASClientDefaultImp parameters are not valid!");
     }
 
     @Test
-    public void MAASClientDefaultImpTest_Throws_InvalidParameterException_If_Given_ModelTransformerClient_Parameter_Is_Null() {
+    public void MAASClientDefaultImp_Throws_InvalidParameterException_If_Given_ModelTransformerClient_Parameter_Is_Null() {
         // Arrange
-        ArrowheadHelper arrowheadHelper = Mockito.mock(ArrowheadHelper.class);
-        RepositoryManagerClient repositoryManagerClient = Mockito.mock(RepositoryManagerClient.class);
-        ModelFilterClient modelFilterClient = Mockito.mock(ModelFilterClient.class);
+        ArrowheadHelper mockArrowheadHelper = Mockito.mock(ArrowheadHelper.class);
+        RepositoryManagerClient mockRepositoryManagerClient = Mockito.mock(RepositoryManagerClient.class);
+        ModelFilterClient mockModelFilterClient = Mockito.mock(ModelFilterClient.class);
         ModelCrawlerClient modelCrawlerClient = Mockito.mock(ModelCrawlerClient.class);
-        String systemDefinitionListResourcePath = "systemDefinitionListResourcePath";
+        String mockSystemDefinitionListResourcePath = "systemDefinitionListResourcePath";
         // Act and Assert
-        Assertions.assertThatThrownBy(() -> new MAASClientDefaultImp(arrowheadHelper, repositoryManagerClient,
-                modelFilterClient, null, modelCrawlerClient, systemDefinitionListResourcePath))
+        Assertions.assertThatThrownBy(() -> new MAASClientDefaultImp(mockArrowheadHelper, mockRepositoryManagerClient,
+                mockModelFilterClient, null, modelCrawlerClient, mockSystemDefinitionListResourcePath))
                 .isInstanceOf(InvalidParameterException.class)
                 .hasMessageContaining("MAASClientDefaultImp parameters are not valid!");
     }
 
     @Test
-    public void MAASClientDefaultImpTest_Throws_InvalidParameterException_If_Given_ModelCrawlerClient_Parameter_Is_Null() {
+    public void MAASClientDefaultImp_Throws_InvalidParameterException_If_Given_ModelCrawlerClient_Parameter_Is_Null() {
         // Arrange
-        ArrowheadHelper arrowheadHelper = Mockito.mock(ArrowheadHelper.class);
-        RepositoryManagerClient repositoryManagerClient = Mockito.mock(RepositoryManagerClient.class);
-        ModelFilterClient modelFilterClient = Mockito.mock(ModelFilterClient.class);
-        ModelTransformerClient modelTransformerClient = Mockito.mock(ModelTransformerClient.class);
-        String systemDefinitionListResourcePath = "systemDefinitionListResourcePath";
+        ArrowheadHelper mockArrowheadHelper = Mockito.mock(ArrowheadHelper.class);
+        RepositoryManagerClient mockRepositoryManagerClient = Mockito.mock(RepositoryManagerClient.class);
+        ModelFilterClient mockModelFilterClient = Mockito.mock(ModelFilterClient.class);
+        ModelTransformerClient mockModelTransformerClient = Mockito.mock(ModelTransformerClient.class);
+        String mockSystemDefinitionListResourcePath = "systemDefinitionListResourcePath";
         // Act and Assert
-        Assertions.assertThatThrownBy(() -> new MAASClientDefaultImp(arrowheadHelper, repositoryManagerClient,
-                modelFilterClient, modelTransformerClient, null, systemDefinitionListResourcePath))
+        Assertions.assertThatThrownBy(() -> new MAASClientDefaultImp(mockArrowheadHelper, mockRepositoryManagerClient,
+                mockModelFilterClient, mockModelTransformerClient, null, mockSystemDefinitionListResourcePath))
                 .isInstanceOf(InvalidParameterException.class)
                 .hasMessageContaining("MAASClientDefaultImp parameters are not valid!");
     }
 
     @Test
-    public void MAASClientDefaultImpTest_Throws_InvalidParameterException_If_Given_SystemDefinitionListResourcePath_Parameter_Is_Null() {
+    public void MAASClientDefaultImp_Throws_InvalidParameterException_If_Given_SystemDefinitionListResourcePath_Parameter_Is_Null() {
         // Arrange
-        ArrowheadHelper arrowheadHelper = Mockito.mock(ArrowheadHelper.class);
-        RepositoryManagerClient repositoryManagerClient = Mockito.mock(RepositoryManagerClient.class);
-        ModelFilterClient modelFilterClient = Mockito.mock(ModelFilterClient.class);
-        ModelTransformerClient modelTransformerClient = Mockito.mock(ModelTransformerClient.class);
-        ModelCrawlerClient modelCrawlerClient = Mockito.mock(ModelCrawlerClient.class);
-        // Act and Assert
-        Assertions.assertThatThrownBy(() -> new MAASClientDefaultImp(arrowheadHelper, repositoryManagerClient,
-                modelFilterClient, modelTransformerClient, modelCrawlerClient, null))
-                .isInstanceOf(InvalidParameterException.class)
-                .hasMessageContaining("MAASClientDefaultImp parameters are not valid!");
-    }
-
-    @Test
-    public void MAASClientDefaultImpTest_Throws_InvalidParameterException_If_Given_SystemDefinitionListResourcePath_Parameter_Is_Empty() {
-        // Arrange
-        ArrowheadHelper arrowheadHelper = Mockito.mock(ArrowheadHelper.class);
-        RepositoryManagerClient repositoryManagerClient = Mockito.mock(RepositoryManagerClient.class);
-        ModelFilterClient modelFilterClient = Mockito.mock(ModelFilterClient.class);
-        ModelTransformerClient modelTransformerClient = Mockito.mock(ModelTransformerClient.class);
+        ArrowheadHelper mockArrowheadHelper = Mockito.mock(ArrowheadHelper.class);
+        RepositoryManagerClient mockRepositoryManagerClient = Mockito.mock(RepositoryManagerClient.class);
+        ModelFilterClient mockModelFilterClient = Mockito.mock(ModelFilterClient.class);
+        ModelTransformerClient mockModelTransformerClient = Mockito.mock(ModelTransformerClient.class);
         ModelCrawlerClient modelCrawlerClient = Mockito.mock(ModelCrawlerClient.class);
         // Act and Assert
-        Assertions.assertThatThrownBy(() -> new MAASClientDefaultImp(arrowheadHelper, repositoryManagerClient,
-                modelFilterClient, modelTransformerClient, modelCrawlerClient, null))
+        Assertions.assertThatThrownBy(() -> new MAASClientDefaultImp(mockArrowheadHelper, mockRepositoryManagerClient,
+                mockModelFilterClient, mockModelTransformerClient, modelCrawlerClient, null))
                 .isInstanceOf(InvalidParameterException.class)
                 .hasMessageContaining("MAASClientDefaultImp parameters are not valid!");
     }
 
     @Test
-    public void MAASClientDefaultImpTest_Registers_SystemsToArrowhead_Via_ArrowheadHelper()
+    public void MAASClientDefaultImp_Throws_InvalidParameterException_If_Given_SystemDefinitionListResourcePath_Parameter_Is_Empty() {
+        // Arrange
+        ArrowheadHelper mockArrowheadHelper = Mockito.mock(ArrowheadHelper.class);
+        RepositoryManagerClient mockRepositoryManagerClient = Mockito.mock(RepositoryManagerClient.class);
+        ModelFilterClient mockModelFilterClient = Mockito.mock(ModelFilterClient.class);
+        ModelTransformerClient mockModelTransformerClient = Mockito.mock(ModelTransformerClient.class);
+        ModelCrawlerClient modelCrawlerClient = Mockito.mock(ModelCrawlerClient.class);
+        // Act and Assert
+        Assertions.assertThatThrownBy(() -> new MAASClientDefaultImp(mockArrowheadHelper, mockRepositoryManagerClient,
+                mockModelFilterClient, mockModelTransformerClient, modelCrawlerClient, null))
+                .isInstanceOf(InvalidParameterException.class)
+                .hasMessageContaining("MAASClientDefaultImp parameters are not valid!");
+    }
+
+    @Test
+    public void MAASClientDefaultImp_Registers_SystemsToArrowhead_Via_ArrowheadHelper()
             throws UnavailableServerException, JsonSyntaxException, IOException {
         // Arrange
-        ArrowheadHelper arrowheadHelper = Mockito.mock(ArrowheadHelper.class);
-        RepositoryManagerClient repositoryManagerClient = Mockito.mock(RepositoryManagerClient.class);
-        ModelFilterClient modelFilterClient = Mockito.mock(ModelFilterClient.class);
-        ModelTransformerClient modelTransformerClient = Mockito.mock(ModelTransformerClient.class);
+        ArrowheadHelper mockArrowheadHelper = Mockito.mock(ArrowheadHelper.class);
+        RepositoryManagerClient mockRepositoryManagerClient = Mockito.mock(RepositoryManagerClient.class);
+        ModelFilterClient mockModelFilterClient = Mockito.mock(ModelFilterClient.class);
+        ModelTransformerClient mockModelTransformerClient = Mockito.mock(ModelTransformerClient.class);
         ModelCrawlerClient modelCrawlerClient = Mockito.mock(ModelCrawlerClient.class);
-        String systemDefinitionListResourcePath = "systemDefinitionListResourcePath";
+        String mockSystemDefinitionListResourcePath = "systemDefinitionListResourcePath";
 
         ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
-        Mockito.doNothing().when(arrowheadHelper).registerSystemsToArrowhead(argumentCaptor.capture());
+        Mockito.doNothing().when(mockArrowheadHelper).registerSystemsToArrowhead(argumentCaptor.capture());
         // Act
-        new MAASClientDefaultImp(arrowheadHelper, repositoryManagerClient,
-                modelFilterClient, modelTransformerClient, modelCrawlerClient, systemDefinitionListResourcePath);
+        new MAASClientDefaultImp(mockArrowheadHelper, mockRepositoryManagerClient,
+                mockModelFilterClient, mockModelTransformerClient, modelCrawlerClient,
+                mockSystemDefinitionListResourcePath);
         // Assert
         boolean callingCount = argumentCaptor.getAllValues().size() == 1;
-        boolean calledParameterIsExpected = argumentCaptor.getAllValues().get(0) == systemDefinitionListResourcePath;
+        boolean calledParameterIsExpected = argumentCaptor.getAllValues()
+                .get(0) == mockSystemDefinitionListResourcePath;
         Assertions.assertThat(callingCount && calledParameterIsExpected).isTrue();
     }
 
+    @Test
+    public void MAASClientDefaultImp_Set_RepositoryManagerClient()
+            throws UnavailableServerException, JsonSyntaxException, IOException {
+        // Arrange
+        ArrowheadHelper mockArrowheadHelper = Mockito.mock(ArrowheadHelper.class);
+        RepositoryManagerClient mockRepositoryManagerClient = Mockito.mock(RepositoryManagerClient.class);
+        ModelFilterClient mockModelFilterClient = Mockito.mock(ModelFilterClient.class);
+        ModelTransformerClient mockModelTransformerClient = Mockito.mock(ModelTransformerClient.class);
+        ModelCrawlerClient modelCrawlerClient = Mockito.mock(ModelCrawlerClient.class);
+        String mockSystemDefinitionListResourcePath = "systemDefinitionListResourcePath";
+        // Act
+        MAASClientDefaultImp imp = new MAASClientDefaultImp(mockArrowheadHelper, mockRepositoryManagerClient,
+                mockModelFilterClient, mockModelTransformerClient, modelCrawlerClient,
+                mockSystemDefinitionListResourcePath);
+        // Assert
+        Assertions.assertThat(imp.getRepositoryManagerClient())
+                .isEqualTo(mockRepositoryManagerClient);
+    }
+
+    @Test
+    public void MAASClientDefaultImp_Set_ModelFilterClient()
+            throws UnavailableServerException, JsonSyntaxException, IOException {
+        // Arrange
+        ArrowheadHelper mockArrowheadHelper = Mockito.mock(ArrowheadHelper.class);
+        RepositoryManagerClient mockRepositoryManagerClient = Mockito.mock(RepositoryManagerClient.class);
+        ModelFilterClient mockModelFilterClient = Mockito.mock(ModelFilterClient.class);
+        ModelTransformerClient mockModelTransformerClient = Mockito.mock(ModelTransformerClient.class);
+        ModelCrawlerClient modelCrawlerClient = Mockito.mock(ModelCrawlerClient.class);
+        String mockSystemDefinitionListResourcePath = "systemDefinitionListResourcePath";
+        // Act
+        MAASClientDefaultImp imp = new MAASClientDefaultImp(mockArrowheadHelper, mockRepositoryManagerClient,
+                mockModelFilterClient, mockModelTransformerClient, modelCrawlerClient,
+                mockSystemDefinitionListResourcePath);
+        // Assert
+        Assertions.assertThat(imp.getModelFilterClient())
+                .isEqualTo(mockModelFilterClient);
+    }
+
+    @Test
+    public void MAASClientDefaultImp_Set_ModelTransformerClient()
+            throws UnavailableServerException, JsonSyntaxException, IOException {
+        // Arrange
+        ArrowheadHelper mockArrowheadHelper = Mockito.mock(ArrowheadHelper.class);
+        RepositoryManagerClient mockRepositoryManagerClient = Mockito.mock(RepositoryManagerClient.class);
+        ModelFilterClient mockModelFilterClient = Mockito.mock(ModelFilterClient.class);
+        ModelTransformerClient mockModelTransformerClient = Mockito.mock(ModelTransformerClient.class);
+        ModelCrawlerClient modelCrawlerClient = Mockito.mock(ModelCrawlerClient.class);
+        String mockSystemDefinitionListResourcePath = "systemDefinitionListResourcePath";
+        // Act
+        MAASClientDefaultImp imp = new MAASClientDefaultImp(mockArrowheadHelper, mockRepositoryManagerClient,
+                mockModelFilterClient, mockModelTransformerClient, modelCrawlerClient,
+                mockSystemDefinitionListResourcePath);
+        // Assert
+        Assertions.assertThat(imp.getModelTransformerClient())
+                .isEqualTo(mockModelTransformerClient);
+    }
+
+    @Test
+    public void MAASClientDefaultImp_Set_ModelCrawlerClient()
+            throws UnavailableServerException, JsonSyntaxException, IOException {
+        // Arrange
+        ArrowheadHelper mockArrowheadHelper = Mockito.mock(ArrowheadHelper.class);
+        RepositoryManagerClient mockRepositoryManagerClient = Mockito.mock(RepositoryManagerClient.class);
+        ModelFilterClient mockModelFilterClient = Mockito.mock(ModelFilterClient.class);
+        ModelTransformerClient mockModelTransformerClient = Mockito.mock(ModelTransformerClient.class);
+        ModelCrawlerClient modelCrawlerClient = Mockito.mock(ModelCrawlerClient.class);
+        String mockSystemDefinitionListResourcePath = "systemDefinitionListResourcePath";
+        // Act
+        MAASClientDefaultImp imp = new MAASClientDefaultImp(mockArrowheadHelper, mockRepositoryManagerClient,
+                mockModelFilterClient, mockModelTransformerClient, modelCrawlerClient,
+                mockSystemDefinitionListResourcePath);
+        // Assert
+        Assertions.assertThat(imp.getModelCrawlerClient())
+                .isEqualTo(modelCrawlerClient);
+    }
 }
