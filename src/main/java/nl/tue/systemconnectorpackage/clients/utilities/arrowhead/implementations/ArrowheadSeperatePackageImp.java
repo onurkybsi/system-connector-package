0cba1ac7341ecdeef19f05e2ca6664c305cb0a9c
onurkybsi <onurbpm@outlook.com>
2021-12-15 11:44:47 +0100

    Redundant configuration was removed

--- src/main/java/nl/tue/systemconnectorpackage/clients/utilities/arrowhead/implementations/ArrowheadSeperatePackageImp.java
+++ src/main/java/nl/tue/systemconnectorpackage/clients/utilities/arrowhead/implementations/ArrowheadSeperatePackageImp.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import nl.tue.systemconnectorpackage.clients.services.systemInformation.SystemInformationService;
 import org.apache.logging.log4j.LogManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
